8300251acab93272b65c8d89216264a59e2380bf
Pierre Guilbault <guilbaultpj@gmail.com>
2021-06-29 07:24:04 -0400

    add validation arry for register, add to utils file and try and flesh out some routes for register get

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const { loginUser, logoutUser, requireAuth } = require('../auth');
 const db = require('../db/models');
 const { User } = db;
 const { csrfProtection, asyncHandler, check, handleValidationErrors } = require('./utils');
+const { validationResult } = require('express-validator');
 
 const validateEmailAndPassword = [
   check('email')
@@ -17,32 +18,82 @@ const validateEmailAndPassword = [
   handleValidationErrors,
 ]; //TODO add validators for special characters
 
+const validateUsers = [
+  check('username')
+    .exists({checkFalsy: true})
+    .withMessage('Please enter a username for your WhatsNext account')
+    .isLength({ max: 25 })
+    .withMessage('Please keep your username under 25 characters in length'),
+  check('firstName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please enter your first name')
+    .isLength({ max: 15 })
+    .withMessage('My goodness you have a long name, please enter an abbreviation or your nickname'),
+  check('lastName')
+    .exists({ checkFalsy: true })
+    .withMessage('Please enter your last name')
+    .isLength({ max: 30 })
+    .withMessage('I\'m sorry that your name is so long, please provide the first 30 characters of your name'),
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage('Please fill out the email field')
+    .isLength({ max: 255 })
+    .withMessage('Please don\'t tell us you really have an email that long')
+    .isEmail()
+    .withMessage('Email Address needs to be an actual email address, check you have a @ and a TLD i.e. .com, .org, .gov')
+    .custom((value) => {
+      return User.findOne({
+        where: { email: value }
+      })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('Sorry this email is already in use, perhaps you already have an account')
+          }
+        });
+    }),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please enter a password')
+      .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*-]{8,30})/)
+      .withMessage(`Please create a password between 8 and 30 characters in length
+                    with at least one lower case letter, one upper case letter,
+                    one number, and one special character`),
+    check('confirmPassword')
+      .exists({ checkFalsy: true })
+      .withMessage('Please fill out the confirm password field')
+      .custom((value, { req }) => {
+        if (value !== req.body.password) {
+          throw new Error('Your Confirm Password does not match your Password');
+        }
+        return true;
+      }),
+      handleValidationErrors,
+];
 
 /* GET users listing. */
 router.get('/', requireAuth, (req, res) => {
-    console.log(`bunch of nonsense`)
-    const lilVariable = 'ah string'
-    res.json({lilVariable})
+  console.log(`bunch of nonsense`)
+  const lilVariable = 'ah string'
+  res.json({lilVariable})
  // res.send('respond with a resource');
 });
 
 
-
-router.get('/login', 
-   csrfProtection,
+router.get('/login',
+  csrfProtection,
   asyncHandler( async(req, res, next) => {
 
-    res.json({ csrfToken: req.csrfToken() })
-    // res.render('login', {
+  res.json({ csrfToken: req.csrfToken() })
+  // res.render('login', {
     //   csrfToken: req.csrfToken(),
     // });
-  }));
+}));
 
-router.post('/login', 
-  // csrfProtection, 
-  validateEmailAndPassword,
-  asyncHandler( async(req, res, next) => {
-    console.log('Hello /users/login route');
+router.post('/login',
+// csrfProtection,
+validateEmailAndPassword,
+asyncHandler( async(req, res, next) => {
+  console.log('Hello /users/login route');
   const {
     email,
     password
@@ -55,30 +106,56 @@ router.post('/login',
   const passwordMatch = await bcrypt.compare(password, user.hashedPassword.toString());
   if (passwordMatch) {
     loginUser(req, res, user);
-   // return res.redirect('/');  // TODO change to '/account' which is the users page
+    // return res.redirect('/');  // TODO change to '/account' which is the users page
   }
 
   res.json({user, passwordMatch})
 }));
 
+router.get('/register',
+  csrfProtection,
+  (req, res) => {
+    const user = User.build();
+    res.render('whateverthepugis', {
+      title: 'Register',
+      user,
+      csrfToken: req.csrfToken(),
+    });
+  }
+);
 
-
-router.post('/register', 
-//csrfProtection, 
-asyncHandler( async (req, res) => {
+router.post('/register',
+  validateUsers,
+  //csrfProtection,
+  asyncHandler( async (req, res) => {
     const { username, firstName, lastName, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = User.build({ username, firstName, lastName, email, hashedPassword });
-    res.json({user})
-}))
 
+    const validateErrors = validationResult(req);
 
+    if (validationResult.isEmpty()) {
+      await user.save();
+      loginUser(req, res, next);
+      res.redirect('/account');
+    } else {
+      console.log('You\'ve got Errs!!!!')
+      const errors = validateErrors.array().map((error) => error.msq);
+        // TODO render the user login pug
+        /*
+        title: 'Login',
+        email,
+        errors,
+        csrfToken: req.csrfToken(),
+        */
+    }
 
-
+    res.status(201).json({user});
+}));
 
 
 router.post('/logout', (req, res) => {
     logoutUser(req, res);
     res.redirect('/');
-})
+});
 module.exports = router;
--- routes/utils.js
+++ routes/utils.js
@@ -2,9 +2,21 @@ const csrf = require('csurf');
 const { validationResult, check } = require('express-validator');
 
 const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
+
 const csrfProtection = csrf({cookie: true});
+
 const handleValidationErrors = (req, res, next) => {
     const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        console.log('You\'ve got mail ===>');
+        const errors = validationErrors.array().map((error) => error.msg);
+
+        const err = Error('Error Error Will Robinson');
+        err.status = 400;
+        err.title = 'Bad Robot.';
+        err.errors = errors;
+        return next(err);
+    }
     next();
 };
 
