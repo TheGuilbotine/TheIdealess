ae665708667f47be5871fa002faf0f8e6ab8ba6d
Nico Pierson <nicogpt@gmail.com>
2021-06-28 20:35:27 -0700

    added lists and task seed data

--- db/seeders/20210629032522-add-task-type.js
+++ db/seeders/20210629032522-add-task-type.js
@@ -2,25 +2,14 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      return queryInterface.bulkInsert('TaskTypes', [
+        { taskType: 'pre-travel', createdAt: new Date(), updatedAt: new Date() },
+        { taskType: 'travel', createdAt: new Date(), updatedAt: new Date() },
+        { taskType: 'post-travel', createdAt: new Date(), updatedAt: new Date() },
+      ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('TaskTypes', null, {});
   }
 };
--- db/seeders/20210629032531-add-list.js
+++ db/seeders/20210629032531-add-list.js
@@ -2,25 +2,15 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('Lists', [
+      { listName: 'Pre Travel Checklist', userId: 1, createdAt: new Date(), updatedAt: new Date() },
+      { listName: 'What to do in LA', userId: 2, createdAt: new Date(), updatedAt: new Date() },
+      { listName: '10 common Portuguese Phrases!', userId: 3, createdAt: new Date(), updatedAt: new Date() },
+      { listName: 'Food I love', userId: 4, createdAt: new Date(), updatedAt: new Date() },
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete('Lists', null, {});
   }
 };
