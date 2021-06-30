c86774e7b628a7513d106beafdb3ac8dd4ad67fd
Taylor <digitalidentity21@gmail.com>
2021-06-30 11:12:46 -0700

    tab name adjusted

--- db/seeders/20210628213033-add-user.js
+++ db/seeders/20210628213033-add-user.js
@@ -8,6 +8,7 @@ module.exports = {
       { username: 'sashimi_please', firstName: 'Pudds', lastName: 'The Cat', email: 'sleepallday@yahoo.com', hashedPassword: '$2a$10$w2udPPTUdbDB7DrzolHEYe.KtIe1YlHCIqoAulVw6PhiJ1vtWs2Pa', createdAt: new Date(), updatedAt: new Date() },
       { username: 'stillGotAHeart', firstName: 'Darth', lastName: 'Vader', email: 'darthvrocks@evil.com', hashedPassword: '$2a$10$2wujc8RQ64T8MlzPh5B4V.EPjF1.IA2O0V5Z5qA9PwRAnokixH4Sq', createdAt: new Date(), updatedAt: new Date() },
       { username: 'littleMissPerfect', firstName: 'Hermione', lastName: 'Granger', email: 'queenslug@hogwarts.edu', hashedPassword: '$2a$10$2IvS3F2alK7ZAZCbdwljW.Vg79KD7Dcn9RiGFcmAuOc17h0l91VdS', createdAt: new Date(), updatedAt: new Date() },
+      
       { username: 'DemoUser', firstName: 'Phineas', lastName: 'Fogg', email: 'around@world.com', hashedPassword: '$2a$10$2IvS3F2alK7ZAZCbdwljW.Vg79KD7Dcn9RiGFcmAuOc17h0l91VdS', createdAt: new Date(), updatedAt: new Date() },
     ], {});
   },
