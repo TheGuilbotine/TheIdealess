c20d30993559594a4be2ed14cfa91421748ce4fd
Nico Pierson <nicogpt@gmail.com>
2021-06-29 22:00:32 -0700

    delete flists works for fetch

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,4 +1,23 @@
+const handleListDelete = (listId) => {
+  return async () => {
+    try {
+      const res = await fetch(`http://localhost:8080/api/lists/${listId}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
+      if (!res.ok) {
+        throw res;
+      }
+
+      document.querySelector(`#list-${listId}`).remove();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
 
 const fetchLists = async () => {
 
@@ -18,7 +37,10 @@ const fetchLists = async () => {
     ({ listName, id }) => `
     <div class='list__container' id='list-${id}'>
       <div class='list__body'>
-        <h4>${listName}</h4>
+        <p class='list__text'>${listName}</p>
+        <button id='${id}' class='list__delete_button btn btn-secondary'>
+          Delete
+        </button>
       </div>
     </div>
     `
@@ -26,6 +48,14 @@ const fetchLists = async () => {
 
   listsContainer.innerHTML = listsHTML.join('');
 
+  // add event listeners to the delete buttons
+  const deleteButtons = document.querySelectorAll('.list__delete_button');
+  console.log(deleteButtons);
+  if (deleteButtons) {
+    deleteButtons.forEach((button) => {
+      button.addEventListener('click', handleListDelete(button.id));
+    });
+  }
 };
 
 document.addEventListener('DOMContentLoaded', async (event) => {
